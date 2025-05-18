0ab05cfce2279cdf6c821e4bb4adc334c419c3e0
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2025-05-18 21:02:37 +0300

    Creating wxWidgets application main window

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <wx/wx.h>
+
+class MainApp : public wxApp {
+public:
+  virtual bool OnInit();
+};
+
+class MainWindow : public wxFrame {
+public:
+  MainWindow(const wxString &title);
+};
+
+wxIMPLEMENT_APP(MainApp);
+
+bool MainApp::OnInit() {
+  MainWindow *mainWindow = new MainWindow("wxOpenGL App");
+  mainWindow->Show(true);
+  return true;
+}
+
+MainWindow::MainWindow(const wxString &title)
+    : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxSize(800, 600)) {}
